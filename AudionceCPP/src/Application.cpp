debe3aae09729a3ed507c4f93b861c7775d702a4
mdrobnjak <42944601+mdrobnjak@users.noreply.github.com>
2020-02-23 14:51:56 -0500

    updates

--- AudionceCPP/src/Application.cpp
+++ AudionceCPP/src/Application.cpp
@@ -1,6 +1,14 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtc\type_ptr.hpp>
+
 #include <iostream>
 #include <fstream>
 #include <string>
